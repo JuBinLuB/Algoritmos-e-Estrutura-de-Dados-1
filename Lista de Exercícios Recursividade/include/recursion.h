878d824413f8ff5e2ce490101c70ec30ffad8c7f
Alexssander Fernandes <alexssander84@hotmail.com>
2024-03-10 20:52:44 -0300

    new file:   Makefile 	new file:   apps/main.c 	new file:   bin/main.exe 	new file:   "docs/Lista de Exerc\303\255cios 2 - Recursividade.pdf" 	new file:   include/recursion.h 	new file:   lib/librecursion.a 	new file:   obj/recursion.o 	new file:   src/recursion.c 	deleted:    "Lista de Exerc\303\255cios 2 - Recursividade.pdf" 	deleted:    package/Makefile 	deleted:    package/apps/main.c 	deleted:    package/bin/main.exe 	deleted:    package/include/recursion.h 	deleted:    package/lib/librecursion.a 	deleted:    package/obj/recursion.o 	deleted:    package/src/recursion.c

---
+++ Lista de Exercícios Recursividade/include/recursion.h
@@ -0,0 +1,20 @@
+#ifndef RECURSION_H_INCLUDED
+#define RECURSION_H_INCLUDED
+
+int recursiveSum(int *vector, int size);
+
+void biggerElement(int *vector, int size, int *bigger);
+
+int strLength(char str[], int index);
+
+void reverseOrder(char str[], int start, int end);
+
+int substrOf(char *s, char *t, int index, int sizeT);
+
+int substrIndex(char s[], char t[], int index, int sizeT);
+
+int MDC(int n, int m);
+
+float RecursiveDiv(int p, int q);
+
+#endif // RECURSION_H_INCLUDED
---
+++ Lista de Exercícios Recursividade/src/recursion.c
@@ -0,0 +1,100 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "recursion.h"
+
+// Questao #09
+int recursiveSum(int *vector, int size) {
+    if (size == 0) {
+        return 0;
+    } else {
+        return vector[size - 1] + recursiveSum(vector, size - 1);
+    }
+}
+
+// Questao #10
+void biggerElement(int *vector, int size, int *bigger) {
+    if (size == 0) {
+        return;
+    } else {
+        if (vector[size - 1] > *bigger) {
+            *bigger = vector[size - 1];
+        }
+        biggerElement(vector, size - 1, bigger);
+    }
+}
+
+// Questao #11
+int strLength(char str[], int index) {
+    if (str == NULL) {
+        return 0;
+    } else {
+        if (str[index] != '\0') {
+            return strLength(str, index + 1) + 1;
+        } else {
+            return 0;
+        }
+    }
+}
+
+// Questao #12
+void reverseOrder(char str[], int start, int end) {
+    int aux;
+
+    if (str == NULL) {
+        return;
+    } else {
+        if (start < end - 1) {
+            aux = str[start];
+            str[start] = str[end - 1];
+            str[end - 1] = aux;
+            reverseOrder(str, start + 1, end - 1);
+        }
+    }
+}
+
+// Questao #13
+int substrOf(char *s, char *t, int index, int sizeT) {
+    if (strncmp(t, s + index, sizeT) == 0) {
+        return 1;
+    } else {
+        if (s[index] == '\0') {
+            return 0;
+        } else {
+            return substrOf(s, t, index + 1, sizeT);
+        }
+    }
+}
+
+// Questao #14
+int substrIndex(char s[], char t[], int index, int sizeT) {
+    if (strncmp(t, s + index, sizeT) == 0) {
+        return index;
+    } else {
+        if (s[index] == '\0') {
+            return -1;  // Retorna -1 indicando que a string "t" não é substring da string "s".
+        } else {
+            return substrIndex(s, t, index + 1, sizeT) + 0;
+        }
+    }
+}
+
+// Questao #15
+int MDC(int n, int m) {
+    if (m <= n && n % m == 0) {
+        return m;
+    } else if (n < m) {
+        return MDC(m, n);
+    } else {
+        return MDC(m, n % m);
+    }
+}
+
+// Questao #16
+float RecursiveDiv(int p, int q) {
+    if (p < q) {
+        return 0;
+    } else {
+        return RecursiveDiv(p - q, q) + 1;
+    }
+}
